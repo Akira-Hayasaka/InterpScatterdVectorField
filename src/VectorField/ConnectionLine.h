4cfff7593b710a29b750ffc485f73d1c43b6d17c
Akira <Akira@Akira-no-MacBook-Pro.local>
2014-10-05 13:10:38 +0900

    dir

--- src/VectorField/ConnectionLine.h
+++ src/VectorField/ConnectionLine.h
@@ -11,6 +11,7 @@
 
 #include "ofMain.h"
 #include "WindAndPosition.h"
+#include "MSAInterpolator.h"
 
 class ConnectionLine
 {
@@ -31,7 +32,32 @@ public:
         line = line.getResampledByCount(numSample);
     }
     
-    void interpolateWind();
+    void interpolateWind()
+    {
+        msa::Interpolator2D spline2D;
+        spline2D.clear();
+        msa::InterpolationType interpolationType = msa::kInterpolationCubic;
+        spline2D.setInterpolation(interpolationType);
+        
+        vector<WindAndPosition> wpHolder = wps;
+        wps.clear();
+        for (auto wp : wpHolder)
+        {
+            spline2D.push_back(wp.wind);
+        }
+        
+        float step = 1.0 / (line.getVertices().size()-1);
+        int lineIdx = 0;
+        for (float i = 0.0; i <= 1.0; i += step)
+        {
+            ofVec2f wind = spline2D.sampleAt(i);
+            WindAndPosition wp;
+            wp.wind = wind;
+            wp.pos = line.getVertices().at(lineIdx);
+            wps.push_back(wp);
+            lineIdx++;
+        }
+    }
     
     void draw()
     {
